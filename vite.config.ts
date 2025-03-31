9c85e17356727f8da4f88522573804c1e5ab00bd
katya-ignatyk <katuxa.ignatuk@gmail.com>
2025-03-31 11:02:48 +0300

    feat: added import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      api: path.resolve(__dirname, "src/api"),
+      lib: path.resolve(__dirname, "src/lib"),
+      pages: path.resolve(__dirname, "src/pages"),
+      components: path.resolve(__dirname, "src/components"),
+    },
+  },
+  json: {
+    stringify: true,
+  },
 });
